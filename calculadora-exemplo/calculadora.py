a2deedf3a300bd788c6889700655ee4ab184ffd2
Martins-Alonso <66711870+Martins-Alonso@users.noreply.github.com>
2020-06-16 16:04:00 -0400

    mudeio o readme(Parte 2)
    Fiz a estrutura com base em algumas aulas assistidas

--- calculadora-exemplo/calculadora.py
+++ calculadora-exemplo/calculadora.py
@@ -1,2 +1,15 @@
-cal=int(input('Digite o primeiro número:'))
+#Calculadora Simples
+
+#Primeiro Nuḿero:
+#Segunda Núnmero:
+#Operação:
+#Resultado:' '
+
+num1= int(input('Digite o primeiro número:'))
+
+op=input('Digite a operação:')
+
+num2= int(input('Digite o segundo número:'))
+
+print(num1,op,num2)
 
